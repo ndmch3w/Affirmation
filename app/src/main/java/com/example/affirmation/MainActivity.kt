c291faae69d99eee1c63cf11da37f9861905bc46
ndmch3w <daiminh.ng.cs@gmail.com>
2023-10-27 14:51:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/affirmation/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.affirmation
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmation.adapter.ItemAdapter
+import com.example.affirmation.data.Datasource
+import com.example.affirmation.ui.theme.AffirmationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
+    }
+}
